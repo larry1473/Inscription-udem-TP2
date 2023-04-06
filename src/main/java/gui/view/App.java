a8f0c8476ece33b407f20879f4cb6be0c1710d84
larry147 <fotsoguiffo8@gmail.com>
2023-04-06 17:14:01 -0400

    gui test

--- src/main/java/gui/view/App.java
+++ src/main/java/gui/view/App.java
@@ -6,24 +6,30 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class App{
+
+    public static class Gui extends Application{
+
+        @Override
+        public void start(Stage arg0) throws Exception {
+            String javaVersion = System.getProperty("java.version");
+            String javaFxVersion = System.getProperty("javafx.version");
+            Label label = new Label("Hello World, " + javaFxVersion + "." + javaFxVersion);
+            Scene scene = new Scene(new StackPane(),640,480);
+            arg0.setScene(scene);
+            arg0.show();
+            // TODO Auto-generated method stub
+            
+        }
+
+    }
     
 
-    @Override
-    public void start(Stage arg0) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javaFxVersion = System.getProperty("javafx.version");
-        Label label = new Label("Hello World, " + javaFxVersion + "." + javaFxVersion);
-        Scene scene = new Scene(new StackPane(),640,480);
-        arg0.setScene(scene);
-        arg0.show();
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
-    }
+   
 
     public static void main(String[] args){
 
-        launch();
+        Application.launch(Gui.class);
 
     }
     
